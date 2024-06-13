926ce42dfea358d981f0d281932a391d1e16e75e
tangentveil <anirudha0789@gmail.com>
2024-06-13 09:35:48 +0530

    Add server files

--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-export default mongoose.model("User", userSchema);
