e0bd0176497b47c10bc20962642e252df5b8b716
tangentveil <anirudha0789@gmail.com>
2024-06-13 10:03:53 +0530

    client

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, signup } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/signup", signup);
-router.post("/login", login);
-
-export default router;
